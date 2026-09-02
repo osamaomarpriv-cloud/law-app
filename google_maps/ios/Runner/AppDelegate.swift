c8a96302f0002dadf9a12fbb3184c35d2f1312d5
louisehsu <louiseh0313@gmail.com>
2026-09-02 12:23:12 -0700

    Migrate custom standalone samples and app clip to UIScene lifecycle

--- google_maps/ios/Runner/AppDelegate.swift
+++ google_maps/ios/Runner/AppDelegate.swift
@@ -1,18 +1,20 @@
-import UIKit
 import Flutter
 import GoogleMaps
+import UIKit
 
-@UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate {
+@main
+@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-
     // TODO: Add your Google Maps API key
     GMSServices.provideAPIKey("YOUR-API-KEY")
 
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
+    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
   }
 }
--- platform_view_swift/ios/Runner/AppDelegate.swift
+++ platform_view_swift/ios/Runner/AppDelegate.swift
@@ -2,42 +2,56 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import UIKit
 import Flutter
+import UIKit
 
-@UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate, PlatformViewControllerDelegate {
+@main
+@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate, PlatformViewControllerDelegate {
   var flutterResult: FlutterResult?
 
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-        let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
-        let channel = FlutterMethodChannel.init(name: "dev.flutter.sample/platform_view_swift", binaryMessenger: controller.binaryMessenger)
-        
-        channel.setMethodCallHandler({
-            (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
-            if ("switchView" == call.method) {
-                self.flutterResult = result
-                
-                let platformViewController = PlatformViewController(nibName: "PlatformViewController", bundle: nil)
-                platformViewController.counter = call.arguments as! Int
-                platformViewController.delegate = self
-                
-                let navigationController = UINavigationController(rootViewController: platformViewController)
-                navigationController.navigationBar.topItem?.title = "Platform View"
-                controller.present(navigationController, animated: true, completion: nil)
-            } else {
-                result(FlutterMethodNotImplemented)
-            }
-        });
-        
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
-    
-    func didUpdateCounter(counter: Int) {
-        flutterResult?(counter)
-    }
+
+  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
+    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
+
+    let registrar = engineBridge.pluginRegistry.registrar(forPlugin: "PlatformViewSwift")
+    let channel = FlutterMethodChannel(
+      name: "dev.flutter.sample/platform_view_swift",
+      binaryMessenger: engineBridge.applicationRegistrar.messenger()
+    )
+
+    channel.setMethodCallHandler({
+      [weak self] (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+      guard let self = self else { return }
+      if "switchView" == call.method {
+        self.flutterResult = result
+
+        let platformViewController = PlatformViewController(nibName: "PlatformViewController", bundle: nil)
+        platformViewController.counter = call.arguments as! Int
+        platformViewController.delegate = self
+
+        let navigationController = UINavigationController(rootViewController: platformViewController)
+        navigationController.navigationBar.topItem?.title = "Platform View"
+
+        let presenter = registrar?.viewController ?? UIApplication.shared.connectedScenes
+          .compactMap { $0 as? UIWindowScene }
+          .flatMap { $0.windows }
+          .first { $0.isKeyWindow }?
+          .rootViewController
+
+        presenter?.present(navigationController, animated: true, completion: nil)
+      } else {
+        result(FlutterMethodNotImplemented)
+      }
+    })
+  }
+
+  func didUpdateCounter(counter: Int) {
+    flutterResult?(counter)
+  }
 }
